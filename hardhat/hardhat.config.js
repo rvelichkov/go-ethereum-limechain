978c8dc2db48c44447a111acadf75a41544fc687
Rodin Velichkov <rodin.velichkov@gmail.com>
2024-08-08 23:35:07 +0300

    [Add Hardhat Project]
    - Initialize Hardhat project structure
    - Add Token contract and corresponding test
    - Include Hardhat configuration file
    - Set up Ignition deployment module for Token
    - Add package.json and package-lock.json for dependencies
    - Include .gitignore file for Hardhat-specific exclusions

---
+++ hardhat/hardhat.config.js
@@ -0,0 +1,13 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+const { vars } = require("hardhat/config");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.24",
+  networks: {
+    devnet: {
+      url: "http://localhost:8545",
+    }
+  }
+};
